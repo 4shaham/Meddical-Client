932c52ed20ed09236f05a79317cfdefda9d7eda3
4shaham <143824921+4shaham@users.noreply.github.com>
2024-06-11 17:19:32 +0530

    otp verification page is completed

--- src/components/User/OtpComponent.tsx
+++ src/components/User/OtpComponent.tsx
@@ -66,50 +66,66 @@ function OtpComponent() {
 
   const renderInput = () => {
     return Object.keys(otp).map((key, index) => (
-      <input
-        key={index}
-        ref={(element) => {
-          if (element) inputRef.current[index] = element;
-        }}
-        maxLength={1}
-        type="text"
-        name={key}
-        className="bg-white w-16 h-14 rounded-md text-center text-2xl"
-        onChange={(event) => handleChange(event, index)}
-        onKeyUp={(event) => handleKeyBack(event, index)}
-      />
+      <div className="w-16 h-16 ">
+        <input
+          className=" w-full h-full flex flex-col items-center justify-center text-center px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
+          type="text"
+          name={key}
+          id=""
+          key={index}
+          ref={(element) => {
+            if (element) inputRef.current[index] = element;
+          }}
+          maxLength={1}
+          onChange={(event) => handleChange(event, index)}
+          onKeyUp={(event) => handleKeyBack(event, index)}
+        />
+      </div>
     ));
   };
 
   return (
-    <div className="w-full h-screen">
-      <form
-        className="w-[500px] h-[300px] bg-serviceColors rounded-md mx-auto my-[20%] text-center justify-center py-8"
-        onSubmit={handleSubmit}
-      >
-        <h1 className="text-2xl font-medium">Please fill the OTP</h1>
-        <div className="flex gap-5 mt-10 justify-center">{renderInput()}</div>
-        {buttonStatus ? (
-          <button
-            className="bg-red-700 text-white px-12 py-1 rounded-md mx-4 mt-7"
-            type="submit"
-          >
-            Resend
-          </button>
-        ) : (
-          <>
-            <h1 className="mt-5 mb-3">
-              Timer: <span className="text-red">00:{timer}</span>
-            </h1>
-            <button
-              className="bg-btnColor text-white px-12 py-1 rounded-md mt-5"
-              type="submit"
-            >
-              Submit
-            </button>
-          </>
-        )}
-      </form>
+    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-12">
+      <div className="relative bg-white px-6 pt-10 pb-9 shadow-xl mx-auto w-full max-w-lg rounded-2xl">
+        <div className="mx-auto flex w-full max-w-md flex-col space-y-16">
+          <div className="flex flex-col items-center justify-center text-center space-y-2">
+            <div className="font-semibold text-3xl">
+              <p>Email Verification</p>
+            </div>
+            <div className="flex flex-row text-sm font-medium text-gray-400">
+              <p>We have sent a code to your email ba**@dipainhouse.com</p>
+            </div>
+          </div>
+
+          <div>
+            <form action="" method="post">
+              <div className="flex flex-col space-y-5">
+                <div className="flex flex-row items-center justify-between mx-auto w-full max-w-xs">
+                  {renderInput()}
+                </div>
+                <div className="flex flex-col ">
+                 {!buttonStatus?( 
+                  <>
+                    <h1 className="text-center text-red-600 mb-2">Timer:00:{timer}</h1>
+                  <div>
+                    <button className="flex flex-row items-center justify-center text-center w-full border rounded-xl outline-none py-5 bg-btnColor border-none text-white text-sm shadow-sm">
+                      Verify Account
+                    </button>
+                  </div>
+                  </>):(<div>
+                    <button className="flex flex-row items-center justify-center text-center w-full border rounded-xl outline-none py-5 bg-red-500 border-none text-white text-sm shadow-sm">
+                      Resend Otp
+                    </button>
+                  </div>)}
+
+               
+                 
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
