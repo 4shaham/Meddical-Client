5268d534c7a60a1ddaaf7a679f53955c399559c6
4shaham <143824921+4shaham@users.noreply.github.com>
2024-08-16 16:04:52 +0530

    changed in package json

--- src/components/Doctor/VedioCall.tsx
+++ src/components/Doctor/VedioCall.tsx
@@ -1,53 +1,53 @@
-// import React from 'react'
-// import  {ZegoUIKitPrebuilt}  from "@zegoCloud/zego-uikit-prebuilt";
-// import { useLocation } from 'react-router-dom';
+import React from 'react'
+import  {ZegoUIKitPrebuilt}  from "@zegoCloud/zego-uikit-prebuilt";
+import { useLocation } from 'react-router-dom';
 
 
 
-// function VedioCall() {
+function VedioCall() {
 
-//     const location = useLocation();
-//     const searchParams = new URLSearchParams(location.search);
-//     const query: string | null = searchParams.get("roomId");  
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const query: string | null = searchParams.get("roomId");  
    
-//     const roomId=query
+    const roomId=query
 
-//     const myMettings=async(element:any)=>{
+    const myMettings=async(element:any)=>{
 
         
-//         if(!roomId){
-//             return
-//         }
+        if(!roomId){
+            return
+        }
 
-//         const appId=402039914
-//         const serverSecret:string="0e06f7dcb9aa7d5b3148bb58867134ad"
-//         const kitToken=ZegoUIKitPrebuilt.generateKitTokenForTest(appId,serverSecret,roomId,Date.now().toString(),"jk")
+        const appId=402039914
+        const serverSecret:string="0e06f7dcb9aa7d5b3148bb58867134ad"
+        const kitToken=ZegoUIKitPrebuilt.generateKitTokenForTest(appId,serverSecret,roomId,Date.now().toString(),"jk")
 
-//         const zc=ZegoUIKitPrebuilt.create(kitToken)
-//         zc.joinRoom({
-//             container:element,
-//             sharedLinks:[
-//                 {
-//                     name:'Copy Link',
-//                     url:`http://localhost:5173/vedioCallRoom?roomId=${roomId}`
-//                 }
-//             ],
-//             scenario:{
-//                 mode:ZegoUIKitPrebuilt.OneONoneCall
-//             },
-//             showScreenSharingButton:false
-//         })
-//     }
+        const zc=ZegoUIKitPrebuilt.create(kitToken)
+        zc.joinRoom({
+            container:element,
+            sharedLinks:[
+                {
+                    name:'Copy Link',
+                    url:`http://localhost:5173/vedioCallRoom?roomId=${roomId}`
+                }
+            ],
+            scenario:{
+                mode:ZegoUIKitPrebuilt.OneONoneCall
+            },
+            showScreenSharingButton:false
+        })
+    }
 
-//   return (
-//     <div>
-//         <div ref={myMettings} />
-//     </div>
-//   )
+  return (
+    <div>
+        <div ref={myMettings} />
+    </div>
+  )
 
-// }
+}
 
-// export default VedioCall
+export default VedioCall
 
 
 
